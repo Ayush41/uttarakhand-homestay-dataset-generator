7684f3f5f08ed4dc7c9dc685368a1856bd88555d
Ayush Raj <74952106+Ayush41@users.noreply.github.com>
2026-06-14 22:04:54 +0000

    init code

---
+++ generate_dataset.py
@@ -0,0 +1,53 @@
+import pandas as pd
+import numpy as np
+import random 
+from faker import Faker 
+from datetime import datetime, timedelta
+
+fake = Faker("en_IN")
+
+
+NUM_REVIEWS = 3500
+
+
+districts = [
+    # uttarakhands all districts
+    
+]
+
+homestays = [
+    # homestay names
+]
+
+positive_reviews = [
+    
+]
+
+neutral_reviews = [
+
+]
+
+negative_reviews = [
+
+]
+
+review_topics = [
+
+]
+
+
+guest_types = [
+    "Solo Traveler",
+    "Couple",
+    "Family",
+    "Friends Group",
+    "Adventure Traveler"
+]
+
+records = []
+
+# Generate Synthetic Dataset
+
+# Dataframe records and convert to CSV
+
+# Print df head and len of reviews
